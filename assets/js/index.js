3ff9a1fb461649753d553c16d2b20d65aaa44047
Jmacr0 <jonmacrohon@gmail.com>
2019-11-21 00:38:29 +1100

    linked the nav items with html pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,3 @@
-$(document).ready(function () {
-
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function () {
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-
-    });
-});
-
 function updateTime() {
     $('#time').text(moment().format("dddd, MMMM Do h:mm:ss a"));
 };
